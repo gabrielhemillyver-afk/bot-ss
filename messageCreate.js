7cdb82ccd488873bb883d6081d6a383ee9e83890
gabrielhemillyver-afk <gabrielhemillyver@gmail.com>
2026-05-09 10:42:19 -0400

    Create messageCreate.js

---
+++ messageCreate.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+name: 'messageCreate',
+
+async execute(message, client) {
+
+if (message.author.bot) return
+
+const prefix = '!'
+
+if (!message.content.startsWith(prefix)) return
+
+const args = message.content.slice(prefix.length).trim().split(/ +/)
+
+const commandName = args.shift().toLowerCase()
+
+const command = client.commands.get(commandName)
+
+if (!command) return
+
+command.execute(message, args, client)
+}
+}
